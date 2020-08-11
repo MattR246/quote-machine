2a18557f58d9607e39eb5cb32e0e2f4719086235
MattR246 <49105348+MattR246@users.noreply.github.com>
2020-08-11 10:05:24 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Get Quote from API
+async function getQuote() {
+    showLoadingSpinner();
+    const proxyUrl = 'https://blooming-eyrie-78073.herokuapp.com/';
+    const apiUrl =
+        'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        //If Author is blank, add 'Unknown'
+        data.quoteAuthor
+            ? (authorText.innerText = data.quoteAuthor)
+            : (authorText.innerText = 'Unknown');
+        //Reduce font size for longer quotes
+        data.quoteText.length > 120
+            ? quoteText.classList.add('long-quote')
+            : quoteText.classList.remove('long-quote');
+        quoteText.innerText = data.quoteText;
+        //Stop loader, show quote
+        removeLoadingSpinner();
+    } catch (error) {
+        //error handling needs improvement
+        getQuote();
+    }
+}
+
+//New Quote
+const tweetQuote = () => {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+};
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuote();
